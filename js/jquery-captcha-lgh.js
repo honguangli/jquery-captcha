3e43b9afb87a15d9057ea4b0e49c832a4451701d
LGH <1947501227@qq.com>
2020-06-19 11:13:34 +0800

    update add some options example 1:clickRefresh option valid code will refresh when the clickRefresh option equal true and you click the canvas and 2:width and height now you can set the canvas's width and height 3:font you can set the canvas's font 4:resourceType the component have default resource like upper case letters, lower case letters, and digit. you can configure 'a','A','0', or any combination in them. 5:resourceExtra you can add your resource and then it will mix the resourceType option's resource

--- js/jquery-captcha-lgh.js
+++ js/jquery-captcha-lgh.js
@@ -9,49 +9,77 @@ let Captcha;
   "use strict";
   // 默认配置
   const defaults = {
-    element: null,       // canvas节点
-    length: 4,           // 校验码长度
-    code: [],            // 校验码
-    autoRefresh: true,   // 调用校验接口后是否自动刷新
+    length: 4,                   // 校验码长度
+    code: [],                    // 校验码
+    clickRefresh: true,          // 点击刷新
+    autoRefresh: true,           // 调用校验接口后是否自动刷新
+    width: 100,                  // canvas宽度
+    height: 40,                  // canvas高度
+    font: 'bold 23px 微软雅黑',   // 文本字体样式
+    resourceType: 'aA0',           // 资源类型：a-小写字母、A-大写字母、0-数字，可任意组合
+    resourceExtra: [],             // 额外资源
   };
 
-  const sCode = "A,B,C,E,F,G,H,J,K,L,M,N,P,Q,R,S,T,W,X,Y,Z,1,2,3,4,5,6,7,8,9,0";
-  const aCode = sCode.split(",");
-  const aLength = aCode.length; //获取到数组的长度
-
+  const resourceUpper = ["A","B","C","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","W","X","Y","Z"];
+  const resourceLower = ["a","b","c","e","f","g","h","j","k","l","m","n","p","q","r","s","t","w","x","y","z"];
+  const resourceNumber = ["0","1","2","3","4","5","6","7","8","9"];
+  
   Captcha = function(element, options) {
     const self = this;
     self.options = $.extend(true, defaults, options);
     self.element = element;
+    if (self.options.clickRefresh) {
+      self.element.on('click', function () {
+          self.refresh();
+      });
+    }
     self.refresh();
-    self.element.on('click', function () {
-        self.refresh();
-    });
   };
-
+  // 刷新
   Captcha.prototype.refresh = function() {
     const self = this;
-    const canvas_width = self.element.width();
-    const canvas_height = self.element.height();
-    const canvas = self.element[0]; //获取到canvas的对象，演员
-    const context = canvas.getContext("2d"); //获取到canvas画图的环境，演员表演的舞台
-    canvas.width = canvas_width;
-    canvas.height = canvas_height;
+    
+    let resource = [];
+    let resourceLength = 0;
+    // 合并资源
+    if (self.options.resourceType.length > 0) {
+      if (self.options.resourceType.indexOf('A') !== -1) {
+        resource = resource.concat(resourceUpper);
+      }
+      if (self.options.resourceType.indexOf('a') !== -1) {
+        resource = resource.concat(resourceLower);
+      }
+      if (self.options.resourceType.indexOf('0') !== -1) {
+        resource = resource.concat(resourceNumber);
+      }
+    }
+    if (self.options.resourceExtra.length > 0) {
+      resource = resource.concat(self.options.resourceExtra);
+    }
+    // 配置资源为空
+    if (resource.length === 0) {
+      resource = resourceUpper.concat(resourceLower).concat(resourceNumber)
+    }
+    resourceLength = resource.length;
+    const canvas = self.element[0];                       // 获取canvas对象
+    const context = canvas.getContext("2d");              // 获取canvas环境
+    canvas.width = self.options.width;
+    canvas.height = self.options.height;
 
-    const code = [];
+    const code = [];      
+    context.font = self.options.font;
     for (let i = 0; i < self.options.length; i++) {
-      const j = Math.floor(Math.random() * aLength); //获取到随机的索引值
-      const deg = Math.random() * 30 * Math.PI / 180; //产生0~30之间的随机弧度
-      const txt = aCode[j]; //得到随机的一个内容
-      code.push(txt.toLowerCase());
-      const x = 10 + i * 20; //文字在canvas上的x坐标
-      const y = 20 + Math.random() * 8; //文字在canvas上的y坐标
-      context.font = "bold 23px 微软雅黑";
-
+      const txt = resource[Math.floor(Math.random() * resourceLength)]; // 得到随机的一个资源码
+      code.push(txt);                                 
+      
+      const deg = Math.random() * 30 * Math.PI / 180;               //产生0~30之间的随机弧度
+      const x = 10 + i * 20;                                        // 文本的x坐标
+      const y = 20 + Math.random() * 8;                             // 文本的y坐标
+      
       context.translate(x, y);
       context.rotate(deg);
 
-      context.fillStyle = randomColor();
+      context.fillStyle = self.randomColor();
       context.fillText(txt, 0, 0);
 
       context.rotate(-deg);
@@ -59,32 +87,24 @@ let Captcha;
     }
     self.options.code = code;
     for (let i = 0; i <= 5; i++) {
-      context.strokeStyle = randomColor();
+      context.strokeStyle = self.randomColor();
       context.beginPath();
       // 显示线条
-      context.moveTo(Math.random() * canvas_width, Math.random() * canvas_height);
-      context.lineTo(Math.random() * canvas_width, Math.random() * canvas_height);
+      context.moveTo(Math.random() * self.options.width, Math.random() * self.options.height);
+      context.lineTo(Math.random() * self.options.width, Math.random() * self.options.height);
       // 显示小点
-      const x = Math.random() * canvas_width;
-      const y = Math.random() * canvas_height;
+      const x = Math.random() * self.options.width;
+      const y = Math.random() * self.options.height;
       context.moveTo(x, y);
       context.lineTo(x + 1, y + 1);
       context.stroke();
     }
-    
-    //得到随机的颜色值
-    function randomColor() { 
-      const r = Math.floor(Math.random() * 256);
-      const g = Math.floor(Math.random() * 256);
-      const b = Math.floor(Math.random() * 256);
-      return "rgb(" + r + "," + g + "," + b + ")";
-    }
   };
-
+  // 获取当前验证码
   Captcha.prototype.getCode = function() {
     return this.options.code.join('');
   };
-  
+  // 校验
   Captcha.prototype.valid = function(code) {
     const self = this;
     const ans = code.toString().toLowerCase() === self.getCode().toLowerCase();
@@ -93,4 +113,11 @@ let Captcha;
     }
     return ans;
   };
+  // 获取随机的颜色值
+  Captcha.prototype.randomColor = function() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
+  }
 })($);
